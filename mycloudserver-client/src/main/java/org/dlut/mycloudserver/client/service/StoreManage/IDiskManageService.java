a02fea18afbc95ff0cf9528fa4e6cdf9dab19f96
luojie <luojie715@foxmail.com>
2014-10-27 22:35:47 +0800

    添加接口

---
+++ mycloudserver-client/src/main/java/org/dlut/mycloudserver/client/service/StoreManage/IDiskManageService.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2014 etao.com All right reserved. This software is the
+ * confidential and proprietary information of etao.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with etao.com .
+ */
+package org.dlut.mycloudserver.client.service.StoreManage;
+
+/**
+ * 类IDiskManageService.java的实现描述：TODO 类实现描述
+ * 
+ * @author luojie 2014年10月27日 下午10:33:21
+ */
+public interface IDiskManageService {
+
+}
